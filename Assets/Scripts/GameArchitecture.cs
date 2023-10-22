82eeb827ec8808fc1a6b6d6e34da4f58576f5d8f
AStar <sxw59941513@163.com>
2023-10-22 13:42:18 +0800

    [func] Add Architecture

---
+++ Assets/Scripts/GameArchitecture.cs
@@ -0,0 +1,17 @@
+using QFramework;
+
+namespace Game
+{
+    public class GameArchitecture : Architecture<GameArchitecture>
+    {
+        protected override void Init()
+        {
+            // Utilities
+
+            // Models
+
+            // Systems
+
+        }
+    }
+}
